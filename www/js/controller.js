6066cf13f5e1c07b5c21d98f265f647c4d49f981
madcabbage <madcabbage@gmail.com>
2015-07-26 15:22:21 +0800

    Updated CSS slightly

--- www/js/controller.js
+++ www/js/controller.js
@@ -10,10 +10,10 @@ var controller = {
 	parseLocationWindowContent: function(location){
 		var html = '<div>' + 
 			'<h2>' + location.name + '</h2>' + 
-			'<div><div style="float:left">' + location.contentString + '<br />' + 
+			'<div><div style="float:left; width:60%;">' + location.contentString + '<br />' + 
 			'<a href="' + location.webUrl + '" target="_blank">' + location.web + '</a>' +
 			'</div>' + 
-			'<div style="float:right:"><img src="' + location.img + '" width="200px"/></div>' + 	
+			'<div style="float:right; width:40%;"><img src="' + location.img + '"></div>' + 	
 			'</div>';
 		return html;
 	}
