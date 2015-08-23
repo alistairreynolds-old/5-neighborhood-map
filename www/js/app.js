73d5cb64e5188bc847ca514db5a729b7c01bca61
madcabbage <madcabbage@gmail.com>
2015-08-23 15:14:45 +0800

    Added WORKING google maps error checking (points didn't load with previous method)

--- www/js/app.js
+++ www/js/app.js
@@ -1,2 +1,7 @@
 ko.applyBindings(viewModel);
 viewModel.addPoints();
+window.setTimeout(function() {
+    if (typeof google != 'object' || typeof google.maps != 'object') {
+    	$('body').html('Error loading google maps');
+    }
+}, 2000);
