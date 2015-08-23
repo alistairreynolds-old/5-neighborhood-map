07bfb7c1ea96887d3b9508a631c3c26016fa4278
madcabbage <madcabbage@gmail.com>
2015-08-23 14:55:54 +0800

    Revert 5601caf..6128b71
    This rolls back to commit 5601caf55e5ed001996931e31c82ce36f03808c3.

--- www/js/app.js
+++ www/js/app.js
@@ -1,2 +1,14 @@
-ko.applyBindings(viewModel);
-viewModel.addPoints();
+function init(){
+	ko.applyBindings(viewModel);
+	viewModel.addPoints();
+}
+
+// Set timeout for loading google maps. Credit - http://stackoverflow.com/questions/22923106/google-maps-js-v3-detect-load-failures
+$(function(){
+    var timeoutId;
+    timeoutId = window.setTimeout(function() {
+        $('body').html('Error loading google maps.')
+    }, 2000);
+    var url = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback=init';
+    $.getScript(url, function(){ window.clearTimeout(timeoutId) });
+});
